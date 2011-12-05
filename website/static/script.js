0265990e3d58bbafb6b6d6ca08d391fc89d74fda
Ben Dong <bdong281@gmail.com>
2011-12-05 14:30:55 -0800

    Switched to synchronous JS SDK loading, added quick auth check (not tested yet)

--- website/static/script.js
+++ website/static/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+    FB.getLoginStatus(function(response) {
+        if (response.authResponse) {
+            console.log("Auth Response");
+        } else {
+            console.log("Not logged in");
+        }
+    });
 });
