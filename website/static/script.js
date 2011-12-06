6bc7b941ef340fda7614fdd5b2a25fa17849ce2e
Akash Gupta <akash.gupta1@gmail.com>
2011-12-05 17:13:06 -0800

    Moved uid out of a single function scope

--- website/static/script.js
+++ website/static/script.js
@@ -1,3 +1,5 @@
+var uid = -1;
+
 // Allows us to post to the db
 function post(path, params, method) {
     method = method || "post"; // Set method to post by default, if not specified.
@@ -26,19 +28,21 @@ function onReturn(urls, response) {
     console.log(response);
     var myDate = new Date();
     var dateString = myDate.toString();
-    console.log(dateString);
+
+    
 }
 
-function onAuthenticated() {
+function onAuthenticated(response) {
     console.log(response.authResponse);
 
+    uid = response.authResponse.userID;
     var urls = ["https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash4/230131_10150185516282229_504182228_6642763_1929002_n.jpg"];
 
     var options = {
 		uids: "friends@facebook.com",
 		namespace: "facebook.com",
 		user_auth:
-		    "fb_user:" + response.authResponse.userID + "," +
+		    "fb_user:" + uid + "," +
 		    "fb_oauth_token:" + response.authResponse.accessToken
     };
     var apiKey = "24fa7980be31a5332723ce74780786e6";
@@ -50,7 +54,7 @@ $(document).ready(function() {
     FB.getLoginStatus(function(response) {
         if (response.authResponse) {
             $(".fb-login-button").remove();
-            onAuthenticated();
+            onAuthenticated(response);
         } else {
             console.log("Not logged in");
         }
