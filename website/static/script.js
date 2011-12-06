ff1b7ab97795cea7cb75966a6d2934aea81d1056
Akash Gupta <akashgupta@hkn.eecs.berkeley.edu>
2011-12-05 17:47:43 -0800

    Fixed 500

--- website/static/script.js
+++ website/static/script.js
@@ -2,7 +2,7 @@ var uid = -1;
 
 // Allows us to post to the db
 function post(path, params, method) {
-    method = method || "post"; // Set method to post by default, if not specified.
+    /*method = method || "post"; // Set method to post by default, if not specified.
 
     // The rest of this code assumes you are not using a library.
     // It can be made less wordy if you use one.
@@ -21,11 +21,13 @@ function post(path, params, method) {
 
     document.body.appendChild(form);
     form.submit();
-    document.body.removeChild(form);
+    document.body.removeChild(form);*/
+
+    $.post(path, params);
 }
 
 function getProfilePicURL(id) {
-	return "https://www.facebook.com/" + id;
+    return "https://www.facebook.com/" + id;
 }
 
 function onReturn(urls, response) {
@@ -35,18 +37,18 @@ function onReturn(urls, response) {
 
     console.log(response.status);
     for (var photoIndex in response.photos) {
-		for (var tagIndex in response.photos[photoIndex]["tags"]) {
-			console.log(tagIndex);
-			recogUID = response.photos[photoIndex]["tags"][tagIndex]["uids"][0]["uid"];
-			recogUID = recogUID.substring(0, recogUID.length - 13);
-			var profilePicURL = getProfilePicURL(recogUID);
-			options = {
-				time : dateString,
-				photo : profilePicURL,
-				uid : recogUID
-			}
-			post("http://ghost.eecs.berkeley.edu:8888/add", options, "post");
-		}
+	for (var tagIndex in response.photos[photoIndex]["tags"]) {
+	    console.log(tagIndex);
+	    recogUID = response.photos[photoIndex]["tags"][tagIndex]["uids"][0]["uid"];
+	    recogUID = recogUID.substring(0, recogUID.length - 13);
+	    var profilePicURL = getProfilePicURL(recogUID);
+	    options = {
+		time : dateString,
+		photo : profilePicURL,
+		uid : recogUID
+	    }
+	    post("http://ghost.eecs.berkeley.edu:8888/add", options, "post");
+	}
     }
 }
 
@@ -57,11 +59,11 @@ function onAuthenticated(response) {
     var urls = ["https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash4/230131_10150185516282229_504182228_6642763_1929002_n.jpg"];
 
     var options = {
-		uids: "friends@facebook.com",
-		namespace: "facebook.com",
-		user_auth:
-			"fb_user:" + uid + "," +
-			"fb_oauth_token:" + response.authResponse.accessToken
+	uids: "friends@facebook.com",
+	namespace: "facebook.com",
+	user_auth:
+	"fb_user:" + uid + "," +
+	    "fb_oauth_token:" + response.authResponse.accessToken
     };
     var apiKey = "24fa7980be31a5332723ce74780786e6";
     var faceApi = new Face_ClientAPI(apiKey);
